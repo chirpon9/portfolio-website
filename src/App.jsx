cba0fa20b4ad7e6ae3c53ecd8a0f5ff7da9bb2f6
chirpon9 <126436733+chirpon9@users.noreply.github.com>
2025-05-21 08:11:00 +1000

    added vite to tech stack

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const dialogueInfo = {
   about: 'My name is Marco and I\'m a third year student studying information technology at Monash University! I\'m interested in web development and I play a lot of video games in my free time! I designed this website in the <br/> ' +
       'style of a visual novel! I hope you like it! Monash 大学の情報技術を専攻している3年生のマルコと申します！<br/>ゲームが大好きなので、このようなサイトを作りました。よろしくお願いいたします！',
   work: 'I focus mainly on web/app development and design! The main languages I use are: HTML/CSS, Javascript/Typescript, Python, java, php, mySQL and a little bit of C++.' +
-      'As for technologies I use, I mainly use react, bun, MEAN stack, LAMP stack and learning phoenix! I use figma, excalidraw and balsamiq for design.',
+      'As for technologies I use, I mainly use react, bun, vite, MEAN stack, LAMP stack and learning phoenix! I use figma, excalidraw and balsamiq for design.',
   contact: 'The best way to contact me is through email! I don’t really check social media, so please email me! yuetshinglee@gmail.com. <br/> ' +
            '連絡はメールが一番です！SNSあまりめいないので、メールをお願いします。yuetshinglee@gmail.com'
 };
