431434c765706e9a4ca4de71c141dba749d565a8
chirpon9 <126436733+chirpon9@users.noreply.github.com>
2025-05-21 03:13:48 +1000

    deleted vercel analystics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import BackgroundHandler from './background_handler.jsx'
-import { Analytics } from "@vercel/analytics/next"
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -14,7 +11,6 @@ function App() {
           <div className="top-section">
             <h1>melhen / メルヘン</h1>
             <h3>from the german word "Märchen" meaning "fairy tale"</h3>
-              <Analytics />
           </div>
         </div>
     </>
