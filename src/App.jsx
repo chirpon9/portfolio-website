4d32ecd616b56944ab5605148b708eb34a768b96
chirpon9 <126436733+chirpon9@users.noreply.github.com>
2025-05-21 03:10:54 +1000

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import Wavify from "react-wavify";
 import './App.css'
 import BackgroundHandler from './background_handler.jsx'
+import { Analytics } from "@vercel/analytics/next"
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
           <div className="top-section">
             <h1>melhen / メルヘン</h1>
             <h3>from the german word "Märchen" meaning "fairy tale"</h3>
+              <Analytics />
           </div>
         </div>
     </>
