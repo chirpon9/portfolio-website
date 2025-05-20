1a3a286a3aa03f78d3f3c4b613f6a829d9fc0fd8
chirpon9 <126436733+chirpon9@users.noreply.github.com>
2025-05-21 01:20:31 +1000

    background handler stuff

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -1,36 +1,20 @@
 import { useState } from 'react'
 import Wavify from "react-wavify";
 import './App.css'
+import BackgroundHandler from './background_handler.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="top-section">
-        <div className="white-section">
-          <h1>melhen / marco</h1>
-          <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
+        <BackgroundHandler />
+        <div className="textbox">
+          <div className="top-section">
+            <h1>melhen / メルヘン</h1>
+            <h3>from the german word "Märchen" meaning "fairy tale"</h3>
           </div>
         </div>
-        <div className="wave-container">
-          <Wavify
-            fill="#acd6e8" // light blue
-            paused={false}
-            options={{
-              height: 10,
-              amplitude: 30,
-              speed: 0.25,
-              points: 2,
-            }}
-            className="wave"
-            width="100%"
-          />
-        </div>
-      </div>
     </>
   )
 }
